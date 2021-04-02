ba8d07a6721e9651d67b9a5940b0ae01fe3290a3
kazuysd <k-yasuda@cross-wing.jp>
2021-04-02 20:23:06 +0900

    popup

--- src/systems/super-spawner-system.js
+++ src/systems/super-spawner-system.js
@@ -95,61 +95,62 @@ export class SuperSpawnerSystem {
       else if (state.hovered.object3D.name.indexOf('stage08') !== -1) {
         triggeredFunctions.popup08Click();
       }
-
-
-
     }
 
-    performSpawn(state, grabPath, userinput, superSpawner) {
-      const data = superSpawner.data;
 
-      const spawnedEntity = addMedia(
-        data.src,
-        data.template,
-        ObjectContentOrigins.SPAWNER,
-        null,
-        data.resolve,
-        true,
-        false,
-        data.mediaOptions
-      ).entity;
 
-      // TODO HACK objects become held mid frame here, so if something runs between here and
-      // when the networked component is initialized isMine will throw since data doesnt exist
-      // should probably fix isMine in networked aframe instead of merging this
-      if (spawnedEntity.components.networked && !spawnedEntity.components.networked.data) {
-        spawnedEntity.components.networked.data = {};
-      }
-
-      superSpawner.el.object3D.getWorldPosition(spawnedEntity.object3D.position);
-      superSpawner.el.object3D.getWorldQuaternion(spawnedEntity.object3D.quaternion);
-      spawnedEntity.object3D.matrixNeedsUpdate = true;
-
-      superSpawner.el.emit("spawned-entity-created", { target: spawnedEntity });
-
-      state.held = spawnedEntity;
-
-      superSpawner.activateCooldown();
-      state.spawning = true;
-
-      spawnedEntity.addEventListener(
-        "media-loaded",
-        () => {
-          spawnedEntity.object3D.scale.copy(superSpawner.spawnedMediaScale);
-          spawnedEntity.object3D.matrixNeedsUpdate = true;
-          state.spawning = false;
-          superSpawner.el.emit("spawned-entity-loaded", { target: spawnedEntity });
-        },
-        { once: true }
-      );
-    }
-
-    tick() {
-      const interaction = AFRAME.scenes[0].systems.interaction;
-      if (!interaction.ready) return; //DOMContentReady workaround
-      this.maybeSpawn(interaction.state.leftHand, interaction.options.leftHand.grabPath);
-      this.maybeSpawn(interaction.state.rightHand, interaction.options.rightHand.grabPath);
-      this.maybeSpawn(interaction.state.rightRemote, interaction.options.rightRemote.grabPath);
-      this.maybeSpawn(interaction.state.leftRemote, interaction.options.leftRemote.grabPath);
-    }
   }
+
+  performSpawn(state, grabPath, userinput, superSpawner) {
+    const data = superSpawner.data;
+
+    const spawnedEntity = addMedia(
+      data.src,
+      data.template,
+      ObjectContentOrigins.SPAWNER,
+      null,
+      data.resolve,
+      true,
+      false,
+      data.mediaOptions
+    ).entity;
+
+    // TODO HACK objects become held mid frame here, so if something runs between here and
+    // when the networked component is initialized isMine will throw since data doesnt exist
+    // should probably fix isMine in networked aframe instead of merging this
+    if (spawnedEntity.components.networked && !spawnedEntity.components.networked.data) {
+      spawnedEntity.components.networked.data = {};
+    }
+
+    superSpawner.el.object3D.getWorldPosition(spawnedEntity.object3D.position);
+    superSpawner.el.object3D.getWorldQuaternion(spawnedEntity.object3D.quaternion);
+    spawnedEntity.object3D.matrixNeedsUpdate = true;
+
+    superSpawner.el.emit("spawned-entity-created", { target: spawnedEntity });
+
+    state.held = spawnedEntity;
+
+    superSpawner.activateCooldown();
+    state.spawning = true;
+
+    spawnedEntity.addEventListener(
+      "media-loaded",
+      () => {
+        spawnedEntity.object3D.scale.copy(superSpawner.spawnedMediaScale);
+        spawnedEntity.object3D.matrixNeedsUpdate = true;
+        state.spawning = false;
+        superSpawner.el.emit("spawned-entity-loaded", { target: spawnedEntity });
+      },
+      { once: true }
+    );
+  }
+
+  tick() {
+    const interaction = AFRAME.scenes[0].systems.interaction;
+    if (!interaction.ready) return; //DOMContentReady workaround
+    this.maybeSpawn(interaction.state.leftHand, interaction.options.leftHand.grabPath);
+    this.maybeSpawn(interaction.state.rightHand, interaction.options.rightHand.grabPath);
+    this.maybeSpawn(interaction.state.rightRemote, interaction.options.rightRemote.grabPath);
+    this.maybeSpawn(interaction.state.leftRemote, interaction.options.leftRemote.grabPath);
+  }
+}
